80e1995cbbbf9be96140f6e4541e661456af1b93
Yumee Chung <137648897+y0um33@users.noreply.github.com>
2024-09-17 12:20:02 -0600

    Avance1 - shop.h

---
+++ shop.h
@@ -0,0 +1,77 @@
+#ifndef SHOP_H_
+#define SHOP_H_
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include "candy.h"
+
+using namespace std;
+
+const int MAX = 1000;
+
+class Shop {
+private:
+    Candy* can[MAX];
+    int ven;
+
+public:
+    Shop() : ven(0) {}
+
+    void crea_dulce();
+    void muestra_dulce();
+    void agrega_dulce(int calorie, string name, string origin);
+    void sort_calorie();
+    void sort_origin();
+};
+
+void Shop::crea_dulce() {
+    can[ven] = new Candy(50, "Sour Patch", "Canadá");
+    ven++;
+    can[ven] = new Candy(98, "Carlos V", "México");
+    ven++;
+    can[ven] = new Candy(46, "Chupa Chups", "España");
+    ven++;
+    can[ven] = new Candy(100, "HARIBO Goldbears", "Alemania");
+    ven++;
+}
+
+void Shop::muestra_dulce() {
+    for (int i = 0; i < ven; i++)
+        cout << can[i]->to_string() << endl;
+}
+
+void Shop::agrega_dulce(int calorie, string name, string origin) {
+    can[ven] = new Candy(calorie, name, origin);
+    ven++;
+}
+
+//Ordena la caloría en orden ascendente
+//Bubble Sort
+void Shop::sort_calorie() {
+    for (int i = 0; i < ven - 1; i++) {  //ciclo externo: recorre hasta el tamaño-1 del arreglo
+        for (int j = 0; j < ven - i - 1; j++) { //ciclo interno: compara el valor con todos los valores y busca el lugar correspondiente
+            if (can[j]->get_cal() > can[j + 1]->get_cal()) { //si la caloría es menor que el siguiente caloría
+                Candy* temp = can[j]; //realiza este intercambio guardando el valor [j] en variable temp
+                can[j] = can[j + 1]; //igualamos j con el siguiente j+1
+                can[j + 1] = temp;  //guarda j+1 en el variable temp
+            }
+        }
+    }
+}
+
+//Ordena el país del origen en orden alfabético ascendente
+//Bubble Sort
+void Shop::sort_origin() {
+    for (int i = 0; i < ven - 1; i++) { //ciclo externo: recorre hasta el tamaño-1 del arreglo
+        for (int j = 0; j < ven - i - 1; j++) { //ciclo interno: compara el valor con todos los valores y busca el lugar correspondiente
+            if (can[j]->get_origin() > can[j + 1]->get_origin()) { //si el alfabeto origen es menor que el siguiente alfabeto origen
+                Candy* temp = can[j]; //realiza este intercambio guardando el valor [j] en variable temp
+                can[j] = can[j + 1]; //igualamos j con el siguiente j+1
+                can[j + 1] = temp; //guarda j+1 en el variable temp
+            }
+        }
+    }
+}
+
+#endif

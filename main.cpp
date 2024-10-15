0b0d220528dae7b482bc2b5c4f332927445af937
Yumee Chung <137648897+y0um33@users.noreply.github.com>
2024-10-14 19:09:57 -0600

    main.cpp - Avance 2

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <sstream>
 #include "shop.h"
+#include "employee.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    Emplist<string> d1;
     int opcion = 22;
     int answer = 33;
     int respo = 44;
-    Shop tienda; // Asegúrate de especificar que es un vector de punteros a Candy
+    int index;
+    string temp_nombre;
+    int temp_cal;
+    string temp_ori;
+    string nomb;
+
+    Shop tienda;
     tienda.crea_dulce();
+
+    //Agregar en la lista los empleados
+    d1.insertion("Juan");
+    d1.insertion("David");
+    d1.insertion("Leticia");
 
     while (opcion != 0) {
         cout << "BIENVENIDOS A NUESTRA DULCERÍA" << endl;
@@ -19,7 +32,7 @@ int main(int argc, char *argv[]) {
         cout << "Opción: ";
         cin >> opcion;
 
-        if (opcion == 1) {
+        if (opcion == 1) { // Sección para el cliente
             cout << "Gracias por visitar nuestra dulcería." << endl;
             cout << "Estos son los dulces disponibles: " << endl;
             tienda.muestra_dulce();
@@ -30,22 +43,48 @@ int main(int argc, char *argv[]) {
                 cout << "Ordenado por caloría: " << endl;
                 tienda.muestra_dulce();
             }
-        }
-
-        if (opcion == 2) { // Sección para el staff
+        } else if (opcion == 2) { // Sección para el staff
             cout << "Bienvenido, ¿Cuál servicio quieres realizar?" << endl;
             cout << "Agregar productos -> 1" << endl;
             cout << "Agregar empleados -> 2" << endl;
             cout << "Eliminar empleados -> 3" << endl;
             cin >> respo;
-            if (respo == 1) {
-                cout << "Función para agregar productos no implementada." << endl; 
-            }
-            else if (respo == 2) {
-                cout << "Función para agregar empleados no implementada." << endl; 
-            }
-            else if (respo == 3) {
-                cout << "Función para eliminar empleados no implementada." << endl; 
+
+            if (respo == 1) { // Agregar en el vector
+                cout << "Nombre de dulce: " << ' ';
+                cin >> temp_nombre;
+                cout << "Caloría: " << ' ';
+                cin >> temp_cal;
+                cout << "Origen: " << ' ';
+                cin >> temp_ori;
+                tienda.agrega_dulce(temp_cal, temp_nombre, temp_ori);
+                cout << endl;
+                cout << "Lista de dulces modificada: " << endl;
+                tienda.muestra_dulce();
+            } else if (respo == 2) { // Insertar en la lista
+                cout << "Ingresa el nombre del empleado que quieres agregar (SOLO EL NOMBRE): " << endl;
+                cin >> nomb;
+                d1.insertion(nomb);
+                cout << "Ya se agregó" << endl;
+                cout << d1.toString() << endl; 
+            } else if (respo == 3) { // Eliminar en la lista
+                cout << "Lista de empleados: " << endl;
+                cout << d1.toString(); 
+                cout << endl;
+                cout << "Ingresa el número del empleado que quieres eliminar (EL ORDEN QUE ESTÁ REGISTRADO EL EMPLEADO): " << endl;
+                cin >> index; //El índice del objeto dentro de la lista
+
+                if (index >= 0 && index < d1.getSize()) {
+                    d1.deleteAt(index);
+                    cout << "Ya se eliminó" << endl;
+                    cout << "Lista de empleados actualizada: " << endl;
+                    cout << d1.toString();
+                    cout << endl;
+                } else {
+                    cout << "Índice inválido." << endl;
+                }
+            } else {
+                cout << "Oops no sé qué quieres hacer :(" << endl;
             }
         }
     }
